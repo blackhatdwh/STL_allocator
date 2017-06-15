211005167de1485cdb10adef0da45ff9e4de93c6
董玮豪 <3150104577@zju.edu.cn>
2017-06-15 09:18:07 +0800

    add my_test

---
+++ my_test.cpp
@@ -0,0 +1,12 @@
+#include <vector>
+#include <iostream>
+#include "mempool.h"
+
+using namespace std;
+
+int main(){
+    vector<int, MemoryPool<int> > a;
+    for(int i=0;i<4;i++){
+        a.push_back(i);
+    }
+}
